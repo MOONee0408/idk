fafa6f7fc7aa06d5f6755c82624b99c05d5d7f76
MOONee0408 <42657097+MOONee0408@users.noreply.github.com>
2020-05-10 20:49:52 -0500

    Update Prim.py
    YAAAA, ES OFICIAL XD haha

--- Prim.py
+++ Prim.py
@@ -11,16 +11,16 @@ def CrearGrafo(lista):
         grafo.setdefault(e2, []).append(e1)
     return grafo
 
-#función de Prim
+# Prim's
 def mst(inicio, grafo):
-    visitado = set()
+    closed = set()
     aristas = []
     q = [(inicio, inicio)]
     while q:
         v1, v2 = q.pop()
-        if v2 in visitado:
+        if v2 in closed:
             continue
-        visitado.add(v2)
+        closed.add(v2)
         aristas.append((v1, v2))
         for v in grafo[v2]:
             if v in grafo:
@@ -29,38 +29,39 @@ def mst(inicio, grafo):
     assert len(aristas) == len(grafo)-1
     return aristas
 
-    #PARA INGRESAR DATOS DEL ALUMNO:
-def AgregarAlumno():
-    Alum = {}
-    for interes in intereses:
-        #print(interes)
-        n = int(input("Ingrese el puntaje para " + interes + ":\t" ))
-        while (n < 0 or n > 100):
-            n = int(input("¡Ingrese un número válido!\t"))
-        Alum[interes] = n
-    return Alum
 
-
-def imprimirDict(Dict):
-    for clave in Dict:
-        print("Clave:\t " + str(clave) + "\tValor:\t" + str(Dict[clave]))
-
-#Solo he colocado la información para poder ver como realizarlo, estoy pensando que sea con diccionarios
-#sea más facil de trabajar, o la otra es trabajar con lista de listas. 
-#igual quedaría después de ver mas la estructura del programa
 intereses = ['Cfis','Mec', 'Mat', 
              'Cbiosa', 'Eco', 'Ssoc', 
              'Pol', 'Csoc', 'Adm', 'Org', 
-             'ArttVis', 'ExpMus', 'ExpOral','ExpEsc']
+             'ArtVis', 'ExpMus', 'ExpOral','ExpEsc']
 carreras = [['arquitectura'], ['fisica'], ['ingenieria'], ['matematicas'], 
             ['biologia'], ['medicina'], ['psicologia'],['quimica'],
             ['administracion'],['comunicacion'],['derecho'], ['trabajo social'],
             ['diseño grafico'],['historia'],['literatura'],['musica']]
 
-CAreaI = {'arq': {'Mec', 'Mat', 'Org', 'ArttVis'}, 
-          'fis': {'Cfis','Mec', 'Mat'}, 
-          'ing':{'Cfis','Mec', 'Mat'}, 
-          'mat':{'Cfis', 'Mat', 'Org'}}
+
+TArea={'arq': ['Mec', 'Mat', 'Org', 'ArtVis'], 
+          'fis': ['Cfis','Mec', 'Mat'], 
+          'ing':['Cfis','Mec', 'Mat'], 
+          'mat':['Cfis', 'Mat', 'Org'],
+          'bio': {'Cfis', 'Cbiosa', 'Eco', 'Org'}, 
+           'med': {'Cfis', 'Mat', 'Cbiosa', 'Ssoc'}, 
+           'psi':{'Cbiosa','Ssoc', 'Csoc', 'Org', 'ExpOral'}, 
+           'quim':{'Mec', 'Mat', 'Cbiosa', 'Eco'},
+           'adm': {'Mat', 'Adm', 'Org', }, 
+            'com': {'Ssoc', 'Pol', 'Csoc', 'ExpOral','ExpEsc'},
+            'der':{'Ssoc', 'Pol', 'Csoc',  'Org', 'ExpOral','ExpEsc'}, 
+            'trabsoc':{'Eco', 'Ssoc', 'Csoc',  'Org', 'ExpOral','ExpEsc'},
+            'disgraf': {'Pol', 'Csoc', 'ArtVis', 'ExpEsc'}, 
+           'hist': {'Ssoc', 'Pol', 'Csoc', 'ExpOral','ExpEsc'}, 
+           'lit':{'Ssoc', 'ExpOral','ExpEsc'}, 
+           'mus':{'ExpMus', 'ExpOral','ExpEsc'}}
+
+
+CAreaI = {'arq': ['Mec', 'Mat', 'Org', 'ArtVis'], 
+          'fis': ['Cfis','Mec', 'Mat'], 
+          'ing':['Cfis','Mec', 'Mat'], 
+          'mat':['Cfis', 'Mat', 'Org']}
 CAreaII = {'bio': {'Cfis', 'Cbiosa', 'Eco', 'Org'}, 
            'med': {'Cfis', 'Mat', 'Cbiosa', 'Ssoc'}, 
            'psi':{'Cbiosa','Ssoc', 'Csoc', 'Org', 'ExpOral'}, 
@@ -69,37 +70,63 @@ CAreaIII = {'adm': {'Mat', 'Adm', 'Org', },
             'com': {'Ssoc', 'Pol', 'Csoc', 'ExpOral','ExpEsc'},
             'der':{'Ssoc', 'Pol', 'Csoc',  'Org', 'ExpOral','ExpEsc'}, 
             'trabsoc':{'Eco', 'Ssoc', 'Csoc',  'Org', 'ExpOral','ExpEsc'}}
-CAreaIV = {'disgraf': {'Pol', 'Csoc', 'ArttVis', 'ExpEsc'}, 
+CAreaIV = {'disgraf': {'Pol', 'Csoc', 'ArtVis', 'ExpEsc'}, 
            'hist': {'Ssoc', 'Pol', 'Csoc', 'ExpOral','ExpEsc'}, 
            'lit':{'Ssoc', 'ExpOral','ExpEsc'}, 
            'mus':{'ExpMus', 'ExpOral','ExpEsc'}}
 
+Alum={'Cfis': 55,'Mec': 54, 'Mat':66, 
+        'Cbiosa':47, 'Eco': 42, 'Ssoc': 68, 
+        'Pol':42 , 'Csoc': 50, 'Adm': 58, 'Org': 42, 
+        'ArtVis': 48, 'ExpMus': 66, 'ExpOral': 49,'ExpEsc': 72}
 
 
-#Alum={'Cfis': 55,'Mec': 54, 'Mat':66, 'Cbiosa':47, 'Eco': 42, 'Ssoc': 36, 'Pol':42 , 'Csoc': 50, 'Adm': 58, 'Org': 42, 'ArtVis': 62, 'ExpMus': 66, 'ExpOral': 49,'ExpEsc': 72}
+#PARA INGRESAR DATOS DEL ALUMNO:
+def AgregarAlumno():
+    Alum = {}
+    for interes in intereses:
+#        print(interes)
+        n = int(input("Ingrese el puntaje para " + interes + ":\t" ))
+        while (n < 0 or n > 100):
+            n = int(input("¡Ingrese un número válido!\t"))
+        Alum[interes] = n
+    return Alum
+   
+def imprimirDict(Dict):
+    for clave in Dict:
+        print("Clave:\t " + str(clave) + "\tValor:\t" + str(Dict[clave]))
 
-Alum = AgregarAlumno()
+
+def auxi(shasha,num):
+    aux = [shasha, num]
+    return aux
+
+
+#def sera(pts, clave):
+#    if pts == clave:
+#            lista_grafo.append(aux('indef',clave))
+#    else:
+#        pts = pts -1
+#        for k in range(len(areaIV)):
+#            if areaIV[k][1] != clave:
+#                lista_grafo.append(aux(areaIV[k][1],clave))
+
+
+#Alum = AgregarAlumno()
+#copia = Alum.copy()
 Alumno = Alum.copy()
-
-#Elimina todos los valores menores a 45
 for clave in Alum:
     if Alum[clave] < 45:
         Alumno.pop(clave,None)
 
-#También podemos hacer que se agregen todos los valores mayores a 45
-#for clave in Alum:
-    #if Alum[clave] > 45:
-        #Alumno[clave] = Alum[clave]
-#imprimirDict(Alumno)
-        
 
-n = len(Alumno) #Tamaño de nuestro diccionario
+n = len(Alumno) #print(n)
 
-lista_Dict = [] #Esta lista guardará todos los pares de nuestro diccionario.
+lista_Dict = []
 for clave in Alumno:   
-    lista_Dict.append([Alumno[clave], clave]) #Agrega primero valor y luego clave, ej. [[72, 'Ssoc'],...]
+    lista_Dict.append([Alumno[clave], clave])
 
-lista_Dict.sort(reverse=True) #Nos organiza la lista de manera descendente, ej. [[72, 'Ssoc],...,[47, 'Adm']]
+lista_Dict.sort(reverse=True)
 #print("lista:"+ str(lista_Dict))
 
 areaI = []
@@ -126,7 +153,7 @@ for clave in TArea:
     elif clave in CAreaIV:
         definitiva.append([m,4, clave])
         areaIV.append([m, clave])
-        
+
 print("\n\n")    
 definitiva.sort(reverse=True)
 for i in definitiva: 
@@ -134,13 +161,11 @@ for i in definitiva:
 
 maxpts = definitiva[0][0]
 print("Numero maximo de puntos:\t", maxpts)
-lista_grafo = []
-#lista_grafo.append(['indef',definitiva[0][2]])
 
-print("\n\nUltimo ... ")
-contadorAreas = 0
+lista_grafo = []
 pts = maxpts
-#anterior  = definitiva[0][2]
+paso= False
+areaS = []
 anteriores = []
 for i in range(len(definitiva)):
     if definitiva[i][0] == pts: 
@@ -148,16 +173,30 @@ for i in range(len(definitiva)):
         if len(anteriores) == 0:
             lista_grafo.append(['indef',definitiva[i][2]])
             anteriores.append(definitiva[i])
+            c = 1
+            areaS.append(definitiva[i][1])
         elif definitiva[i] not in anteriores and pts ==maxpts:
             lista_grafo.append(['indef',definitiva[i][2]])
             anteriores.append(definitiva[i])
+            c = c +1
+            areaS.append(definitiva[i][1])
             for k in range(len(anteriores)): 
                 if anteriores[k][1] == definitiva[i][1] and anteriores[k][2] != definitiva[i][2]:
-                    lista_grafo.append([anteriores[k][2],definitiva[i][2]])       
-        else:
+                    lista_grafo.append([anteriores[k][2],definitiva[i][2]])
+                    
+        elif pts < maxpts and definitiva[i][1] not in areaS and paso ==False:
             lista_grafo.append(['indef',definitiva[i][2]])
+            c = c + 1
             anteriores.append(definitiva[i])
             for k in range(len(anteriores)): 
+                if anteriores[k][1] == definitiva[i][1] and anteriores[k][2] != definitiva[i][2]:
+                    lista_grafo.append([anteriores[k][2],definitiva[i][2]])
+            if definitiva[i+1][0] != pts:
+                paso = True  
+        else:
+            anteriores.append(definitiva[i])
+            for k in range(len(anteriores)): 
+                
                 if anteriores[k][1] == definitiva[i][1] and anteriores[k][2] != definitiva[i][2]:
                     lista_grafo.append([anteriores[k][2],definitiva[i][2]])
     else:
@@ -167,22 +206,12 @@ for i in range(len(definitiva)):
             if anteriores[k][1] == definitiva[i][1] and anteriores[k][2] != definitiva[i][2]:
                 lista_grafo.append([anteriores[k][2],definitiva[i][2]])
 
-           
-print(lista_grafo)     
+print("\n\nAsociaciones")
+for i in lista_grafo: 
+    print("\n",i)     
 graph = CrearGrafo(lista_grafo)
-print("grafo normal:\n")
+print("\n\ngrafo normal:\n")
 imprimirDict(graph)
 minimo_grafo = CrearGrafo(mst('indef', graph))
 print("grafo minimo:\n")
 imprimirDict(minimo_grafo)
-
-print("\n\n")
-#ESTA PARTE ES APARTE, NOS PRUEBA QUE FUNCIONA LA FUNCIÓN PRIM
-grafo = CrearGrafo([[10, 2], [7, 4], [11, 3], [1, 12], [6, 8], [10, 3], [4, 9], [5, 7], [8, 12], [2, 11], [1, 6], [0, 10], [7, 2], [12, 5]])
-min_grafo = CrearGrafo(mst(0, grafo))
-imprimirDict(min_grafo) 
-#print(min_grafo)
-
-#Crea una lista con los datos.
-lista = [sorted(min_grafo[k]) for k in sorted(min_grafo)]
-print(lista)
